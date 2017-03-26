2d73f409e0156a10b3c4550011fbfa3e544d8520
Xiangmin Kong <rachelkxm@gmail.com>
2017-03-25 22:58:32 -0700

    fix typo

--- js/criterion.js
+++ js/criterion.js
@@ -2,7 +2,7 @@ var crimeRecords = [
     { area: 'Queen Anne',          data: '2,448',  },
     { area: 'Capitol Hill',        data: '2,567',  },
     { area: 'Belltown',            data: '3,298',  },
-    { area: 'Fremont',             data: '3,292',  }
+    { area: 'Fremont',             data: '3,292',  },
     { area: 'Downtown',            data: '3,749',  },
     { area: 'South Lake Union',    data: '3,903',  },
     { area: 'University District', data: '7,153',  },
@@ -12,7 +12,7 @@ var schoolRating = [
   { area: 'University District',          data: '10',  },
   { area: 'Downtown',                     data: '8',  },
   { area: 'South Lake Union',             data: '7',  },
-  { area: 'Fremont',                      data: '6',  }
+  { area: 'Fremont',                      data: '6',  },
   { area: 'Downtown',                     data: '5',  },
   { area: 'South Lake Union',             data: '4',  },
   { area: 'University District',          data: '3',  },
@@ -22,7 +22,7 @@ var transportation = [
   { area: 'Downtown',            data: '10',  },
   { area: 'Capitol Hill',        data: '8',  },
   { area: 'Belltown',            data: '7',  },
-  { area: 'Fremont',             data: '6',  }
+  { area: 'Fremont',             data: '6',  },
   { area: 'Downtown',            data: '5',  },
   { area: 'South Lake Union',    data: '4',  },
   { area: 'University District', data: '3',  },
@@ -32,7 +32,7 @@ var housePrice = [
   { area: 'Downtown',            data: '200',  },
   { area: 'Capitol Hill',        data: '300',  },
   { area: 'Belltown',            data: '400',  },
-  { area: 'Fremont',             data: '700',  }
+  { area: 'Fremont',             data: '700',  },
   { area: 'Downtown',            data: '800',  },
   { area: 'South Lake Union',    data: '900',  },
 ];
