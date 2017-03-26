a687c3406c374d3699b2b26a95dd8f6daa4bf9a5
Xiangmin Kong <rachelkxm@gmail.com>
2017-03-26 13:47:26 -0700

    add the question to suggest list logic

--- js/suggestions.js
+++ js/suggestions.js
@@ -2,7 +2,95 @@
 //add by Neha
 var retrievedObject = localStorage.getItem('survey');
 var survey = JSON.parse(retrievedObject);
+//add by rachel
+//each element is in this format:{"area":"","score":"","detail":{"crime":"","school":"","transportation":"","housePrice":""}}
+var suggestionList = [];
+//question kids mapping
+var suggestedNeighboors;
+if (survey.kids === "YES") {
+  suggestedNeighboors = getNeighboorByCrime();
+   //set the score to the crime less Areas
+   setScoreToNeighboors(suggestedNeighboors,credit.crime,criteria.crime);
+   //set the score to the area with highest school rating
+   suggestedNeighboors  = getNeighboorBySchool();
+   setScoreToNeighboors(suggestedNeighboors,credit.school,criteria.school);
+   console.log(suggestionList);
+}
+//queation income range mapping
+//filter three area with lowest average housing price that over the "housePrice"
+var housePrice = incomeHousePriceMapping[survey.income];
+suggestedNeighboors = getNeighboorByIncome(housePrice);
+setScoreToNeighboors(suggestedNeighboors,credit.housePrice,criteria.housePrice);
 
+//question public transportation mapping
+if(survey.transportation === "DAILY" || survey.transportation === "FEW DAYS IN A WEEK"){
+     suggestedNeighboors = getNeighboorByTransporation();
+     setScoreToNeighboors(suggestedNeighboors,credit.transportation,criteria.transportation);
+}
+//this function to form the suggesting list according to filtered neighboorhood, credit and criteria
+function setScoreToNeighboors(suggestedNeighboors,credit,criteria){
+    var modified = false;
+    for(var n in suggestedNeighboors){
+      var i = 0;
+      for(i = 0;i<suggestionList.length;i++){
+          if(suggestionList[i].area == suggestedNeighboors[n].area){
+            var origScore = parseInt(suggestionList[i].score);
+            suggestionList[i].score = origScore + parseInt(credit)
+            if(typeof suggestionList[i].detail == "undefined")
+                  suggestionList[i].detail = {};
+            suggestionList[i].detail[criteria] = suggestedNeighboors[n].data;
+            modified = true;
+          }
+      }
+      if(!modified){
+          suggestionList[i] = {};
+          suggestionList[i].area = suggestedNeighboors[n].area;
+          suggestionList[i].score = credit;
+          if(typeof suggestionList[i].detail == "undefined")
+               suggestionList[i].detail = {};
+          suggestionList[i].detail[criteria] = suggestedNeighboors[n].data;
+      }else modified = false;
+    }
+}
+function getNeighboorByTransporation(){
+  var transportation = localStorage.getItem("transportation");
+  var transportationObj = JSON.parse(transportation);
+  var neightboors = [transportationObj[0],transportationObj[1],transportationObj[2]];
+  return neightboors;
+}
+//get top three neighboors according to income
+function getNeighboorByIncome(minHousePrice){
+     var housePrice = localStorage.getItem("housePrice");
+     var housePriceObj = JSON.parse(housePrice);
+     var neighboors= [];
+     for(var n in housePriceObj){
+          if(housePriceObj[n].data >= minHousePrice){
+            neighboors[0] = housePriceObj[n];
+            n++;
+            if(n< housePriceObj.length) neighboors[1] = housePriceObj[n];
+            n++;
+            if(n< housePriceObj.length) neighboors[2] = housePriceObj[n];
+            break;
+          }
+     }
+     return neighboors;
+}
+//get top three highest rating school area
+function getNeighboorBySchool(){
+     var schoolRating = localStorage.getItem("schoolRating");
+     var schoolRatingObj = JSON.parse(schoolRating);
+     var neightboors = [schoolRatingObj[0],schoolRatingObj[1],schoolRatingObj[2]];
+     return neightboors;
+}
+//get top three least crime area
+function getNeighboorByCrime(){
+  //{ area: 'Queen Anne',          crimeCount: '2,448',  },
+   var crimeRecords = localStorage.getItem("crimeRecords");
+   var crimeRecordsObj = JSON.parse(crimeRecords);
+   var neighboors = [crimeRecordsObj[0],crimeRecordsObj[1],crimeRecordsObj[2]];
+   return neighboors;
+}
+//end by rachel
 if (survey.kids === "YES") {
     console.log("Suggest Safe Areas");
     var safeAreas = getTop3CrimeRecords();
