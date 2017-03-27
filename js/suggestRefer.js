18780a550ab3c6e1270d321396c4beeed6c4fd42
Xiangmin Kong <rachelkxm@gmail.com>
2017-03-27 15:55:05 -0700

    add neighboorhood.top = 3

--- js/suggestRefer.js
+++ js/suggestRefer.js
@@ -2,18 +2,23 @@
 var neighboorhood = {
   name:         'Seattle',
   number:       '10',
+  top:          '3'
 }
 var credit = {
   crime:           '2',
   school:          '1',
   transportation:  '1',
-  housePrice:      '2'
+  housePrice:      '2',
+  hiking:          '1',
+  swimming:        '1',
+  travelling:      '1'
 }
 var criteria = {
   crime:           'crime',
   school:          'school',
   transportation:  'transportation',
-  housePrice:      'housePrice'
+  housePrice:      'housePrice',
+  supportingAct:   'supportingAct'
 }
 //we can mapping income to house in this way
 /*
