1c62eceeb8c433fcd15e7e1664b247519af64dc1
ng887 <nehagupta887@gmail.com>
2017-04-02 10:36:57 -0700

    update HeaderFooter.js

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,33 +1,11 @@
-/*
-$(document).ready(function(){ 
-  $.get("footer.html", function(data) {
-    $("#footer").html(data);
-  });
-});
-*/
 
-
+/*Header Footer load*/
 $(document).ready(function() {
-    $("#header").load("./header.html");
-    $("#footer").load("./footer.html");
+    $("#header").load("/html/header.html");
+    $("#footer").load("/html/footer.html");
 });
 
-$(document).ready(function() {
-    $("#header").load("./html/header.html");
-    $("#footer").load("./html/footer.html");
-});
-
- /*$("#down").click(function(event) {
-	event.preventDefault();
-    $(this).find("#down-content").toggle();
-});*/
-
+/*drop-dwon toogle */
 $(document).on('click','#down',function(){
-	$(this).find("#down-content").toggle();
+  $(this).find("#down-content").toggle();
 });
-
-/*
-$(document).ready(function() {
-	$('#footer').load('footer.html');
-});*/
-
