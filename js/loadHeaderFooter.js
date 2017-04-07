0a904c02dd5f9f022c2c76ff2270ccbd3a0854e9
Xiangmin Kong <rachelkxm@gmail.com>
2017-04-06 22:04:15 -0700

    add load search

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     $("#header").load("/html/header.html");
     $("#footer").load("/html/footer.html");
+    $("#search").load("/html/findNeighbor.html");
 });
 
 /*drop-dwon toogle */
