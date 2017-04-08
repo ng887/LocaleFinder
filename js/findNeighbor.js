29675f554025608bc1d40376d8e0c78370729b85
Xiangmin Kong <rachelkxm@gmail.com>
2017-04-08 12:23:10 -0700

    change raw dom operation to jquery

--- js/findNeighbor.js
+++ js/findNeighbor.js
@@ -1,38 +1,35 @@
 
 $(function(){
-	$("#btnSearch").click(function(){
-      var searchVal = $("#search-input").val();
-      window.open("../html/neighborDetail.html?name="+searchVal);
-	});
-  	'use strict';
-  	var closeCtrl = document.getElementById('btn-search-close'),
-  		searchContainer = document.querySelector('.search'),
-  		inputSearch = searchContainer.querySelector('.search__input');
+		'use strict';
+		$("#btnSearch").click(function(){
+	      var searchVal = $("#search-input").val();
+	      window.open("../html/neighborDetail.html?name="+searchVal);
+		});
+
+  	var closeCtrl = $('#btn-search-close'),
+  		searchContainer = $('.search'),
+  		inputSearch = $('.search__input');
 
   	function init() {
   		initEvents();
   	}
 
   	function initEvents() {
-  		inputSearch.addEventListener('focus', openSearch);
-  		closeCtrl.addEventListener('click', closeSearch);
+			inputSearch.focus(function(){
+				searchContainer.addClass('search--open');
+			}
+		);
+			closeCtrl.click(function(){
+				searchContainer.removeClass('search--open');
+				inputSearch.blur();
+	  		inputSearch.value = '';
+			});
   		document.addEventListener('keyup', function(ev) {
   			// escape key.
   			if( ev.keyCode == 27 ) {
   				closeSearch();
   			}
   		});
-  	}
-
-  	function openSearch() {
-  		searchContainer.classList.add('search--open');
-  		inputSearch.focus();
-  	}
-
-  	function closeSearch() {
-  		searchContainer.classList.remove('search--open');
-  		inputSearch.blur();
-  		inputSearch.value = '';
   	}
   	init();
 });
