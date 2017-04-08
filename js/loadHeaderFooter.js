143c9cafe4299bfb2f5fc73b76dd00e835081350
Xiangmin Kong <rachelkxm@gmail.com>
2017-04-08 11:06:24 -0700

    bug fix

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -4,18 +4,15 @@ $(document).ready(function() {
     $("#header").load("/html/header.html");
     $("#footer").load("/html/footer.html");
     $("#search").load("/html/findNeighbor.html");
-});
-
-/*drop-dwon toogle */
-$(document).on('click','#down',function(){
-  $(this).find("#down-content").toggle();
-});
-
-/*drop down- sidebar open close*/
-$(document).ready(function lfOpen() {
-    document.getElementById("down-content").style.display = "block";
-});
-
-$(document).ready(function lfClose() {
-    document.getElementById("down-content").style.display = "none";
+    /*drop-dwon toogle */
+    $(document).on('click','#down',function(){
+      $(this).find("#down-content").toggle();
+    });
+    /*drop down- sidebar open close*/
+    function lfOpen() {
+        $("#down-content").style.display = "block";
+    }
+    function lfClose() {
+        $("#down-content").style.display = "none";
+    }
 });
