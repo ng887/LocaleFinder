0b32c7cd714d74ac69b2b7a4fe3eff599aef56e8
NG <gupta.neh@husky.neu.edu>
2017-04-11 11:32:29 -0700

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.lf-like').click(function(){
+        $('.popup').css( "right", "30px" )  ;     
+    });
+
+    $('.close').click(function(){
+        $('.popup').hide();
+    });
+
+});
