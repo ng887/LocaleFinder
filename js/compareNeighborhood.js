811297c9df922e1fff67e6a24339c8bf6686bdf0
Xiangmin Kong <rachelkxm@gmail.com>
2017-04-12 15:35:30 -0700

    add url parse

--- js/compareNeighborhood.js
+++ js/compareNeighborhood.js
@@ -1,7 +1,7 @@
 $(function(){
   var imageMapping ={'University District':'../image/ud.jpg',
     'Queen Anne':           '../image/queenanne.jpg',
-    'South lake Union':     '../image/slu.jpg',
+    'South Lake Union':     '../image/slu.jpg',
     'Ballard':              '../image/ballard.jpg',
     'Fremont':              '../image/fremont.jpg',
     'Downtown':             '../image/downtown.jpg',
@@ -18,5 +18,16 @@ $(function(){
       var rightInput = $(this).val();
       $("#rightImg").attr('src',imageMapping[rightInput]);
     });
-
+    //direct from neighbourhood page
+    $.urlParam = function(name){
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(document.location.href);
+        if (results==null){
+           return null;
+        }
+        else{
+           return results[1] || 0;
+        }
+    };
+    var name = decodeURIComponent($.urlParam("name"));
+    $("#leftInput").val(name);
 });
