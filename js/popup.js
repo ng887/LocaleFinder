d916926d6a24d9414415b02e7892f76ee3f8fcc6
NG <gupta.neh@husky.neu.edu>
2017-04-13 12:18:39 -0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
     $('.lf-like').click(function(){
-        $('.popup').css( "right", "30px" )  ;     
+
+        $('.popup').css( "right", "30px" )  ;   
+       // $('.popup').show();  
     });
 
     $('.close').click(function(){
-        $('.popup').hide();
+       // $('.popup').hide();
+        $('.popup').css( "right", "-380px" )  ;   
     });
 
 });
