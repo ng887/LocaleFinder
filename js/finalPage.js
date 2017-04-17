53e5b650767eeae1ba20867655e6f41e70bd0d66
Xiangmin Kong <rachelkxm@gmail.com>
2017-04-17 00:52:21 -0700

    add url parse

--- js/finalPage.js
+++ js/finalPage.js
@@ -11,7 +11,20 @@
 }); */
 $(document).ready(function()
 {
-	document.getElementById("neighborhoodName").innerHTML = localStorage.getItem('finalSelection') 
+	document.getElementById("neighborhoodName").innerHTML = localStorage.getItem('finalSelection');
+	$.urlParam = function(name){
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(document.location.href);
+		if (results==null){
+		   return null;
+		}
+		else{
+		   return results[1] || 0;
+		}
+	    };
+	    var name = decodeURIComponent($.urlParam("name"));
+	    if(name!="null"){
+		$("#neighborhoodName").html(name);
+	    }
 
 /*$("#choice1").click(function()
     {
