81fe59551725d2474e4af27dfad3a0bb737522ee
rashmithab <bodepudi.r@husky.neu.edu>
2017-04-24 22:31:29 -0700

    final changes

--- js/setter.js
+++ js/setter.js
@@ -20,8 +20,7 @@ $('#rightInput').click(function(){
 });
 function setValues()
 {
-	
-	localStorage.setItem('neighborhoods1', document.getElementById('leftInput').value);
-	localStorage.setItem('neighborhoods2', document.getElementById('rightInput').value);
+	localStorage.setItem('neighborhoods1', document.getElementById('leftInput').value)
+	localStorage.setItem('neighborhoods2', document.getElementById('rightInput').value)
 	window.location.href = "DetailedComparePage.html";
 }
